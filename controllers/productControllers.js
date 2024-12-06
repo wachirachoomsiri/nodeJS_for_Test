973634032eaf60c4ffa98a6a46812039c7055bbf
wachirachoomsiri <wachiragaming@gmail.com>
2024-12-07 05:22:02 +0700

    Update productControllers.js

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -33,7 +33,7 @@ const product_delete = async (req, res) => {
     let { id } = req.params;
     try {
         let { deletedCount } = await Product.deleteOne({ _id: id, 'seller.userId': req.user.userId });
-        if (!deletedCount) {
+        if (deletedCount == 0) {
             return res.status(404).json({
                 status: "Not Found",
             });
